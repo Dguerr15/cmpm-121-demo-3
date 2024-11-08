7457d94448b8add0d7d2464755d4fb1862b49667
Dguerr15 <dguerr15@ucsc.edu>
2024-11-08 15:51:11 -0800

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
-// todo
+// create a button that alerts to screen that it was clicked
+const button = document.createElement("button");
+button.textContent = "Click me!";
+button.onclick = () => alert("You clicked me!");
+document.body.appendChild(button);
